add0d163522ef2431fe2ce7b73a7d423e157a659
Jacob Hiance <jakehiance@gmail.com>
2023-01-21 18:57:00 -0500

    Added multinomial support

--- chessboard_polynomials.py
+++ chessboard_polynomials.py
@@ -1,55 +1,3 @@
-
-"""Load chessboard data from a text file and compute its Rook Polynomial
-
-Reads in chessboard data from a text file.
-
-Only symbols 0 and 1 have meaning. 0 means a restricted cell, and 1 
-represents an available cell. Any other symbols (including spaces) 
-are ignored.
-
-Each row must have the same number of 0s and 1s in order for the 
-board to be loaded properly.
-
-Example: To represent the 3-by-3 board without corners
-  []  
-[][][]
-  []  
-
-The text file needs to look like this:
-010
-111
-010
-
-It could also look like this:
-0 1 0
-1 1 1
-0 1 0
-
-This script will print out the board (for the user's visual inspection), as 
-well as the polynomial with all powers of x shown in ascending order.
-
-For the example above, this script will print the following:
-1x^0 + 5x^1 + 4x^2
-
-Parameters:
-----------
-This script only excepts one command line argument, a .txt file with chessboard data
-
-Example Usage:
---------------
->python chessboard_polynomials.py board_1.txt
-
-Example Output:
----------------
-For the chessboard example above, the output will look like this:
-
-  []
-[][][]
-  []
-1x^0 + 5x^1 + 4x^2
-
-"""
-
 import sys  # sys.argv
 import copy # copy.deepcopy
 
@@ -88,10 +36,25 @@ def count_cells(board):
 
     for row in range(rows):
         for column in range(columns):
-            # Only '1' represents a permissible cell, '0' represents a restricted cell
-            if board[row][column] == '1':
+            # '1', 'R', and 'B' represent permissable cells
+            # Only '0' represents a full restricted cell
+            if board[row][column] != '0':
                 count += 1
     return count
+
+def find_cell(board):
+    rows = len(board)
+    columns = len(board[0])
+
+    for row in range(rows):
+        for column in range(columns):
+            # As soon as we find a cell that's not fully restricted, 
+            # we found a cell we can decompose on
+            if board[row][column] != '0':
+                return [row, column]
+
+    # At this point, there are no vacant, permissible cells
+    return [-1, -1]
 
 def find_first_open_cell(board):
 
@@ -105,16 +68,6 @@ def find_first_open_cell(board):
 
     # At this point, there are no vacant, permissible cells
     return [-1, -1]
-
-def restrict_row(board, row):
-
-    for column in range(len(board[0])):
-        board[row][column] = '0'
-
-def restrict_column(board, column):
-
-    for row in range(len(board)):
-        board[row][column] = '0'
 
 def add_polynomials(polynomial_1, polynomial_2):
 
@@ -133,7 +86,7 @@ def print_board(board):
                 print("  ", end="")
             if board[row][column] == '1':
                 print("[]", end="")
-        # We waqnt to keep each row on the same line of output, so now we can 
+        # We want to keep each row on the same line of output, so now we can 
         # move to the next line
         print("")
 
@@ -144,53 +97,6 @@ def print_polynomial(polynomial):
             print(" + ", end="")
         print(str(polynomial[index]) + "x^" + str(index), end="")
     print("")
-
-def rook_polynomial(board):
-
-    if count_cells(board) == 0:
-        # Base Case: No cells available (1 + 0x = 1)
-        return [1]
-    elif count_cells(board) == 1:
-        # Base Case: Only one cell available (1 + x)
-        return [1, 1]
-    else:
-
-        # We need to find the first available cell for decomposition
-        cell = find_first_open_cell(board)
-
-        # We'll still need to reference this board, so make copies that we 
-        # can modify
-        yes_rook = copy.deepcopy(board)
-        no_rook = copy.deepcopy(board)
-
-        # When placing a rook, restrict it's row and column
-        restrict_row(yes_rook, cell[0])
-        restrict_column(yes_rook, cell[1])
-
-        # When not placing a rook, prevent this cell from being used
-        no_rook[cell[0]][cell[1]] = '0'
-
-        # We'll need the rook polynomials for both sub-boards
-        polynomial_with_rook = rook_polynomial(yes_rook)
-        polynomial_without_rook = rook_polynomial(no_rook)
-
-        # Increment the exponents of the rook polynomial for the board we put a rook on
-        polynomial_with_rook.insert(0, 0)
-
-        # We should make sure both lists have the same length to make adding the polynomials 
-        # easy
-        length_rook = len(polynomial_with_rook)
-        length_no_rook = len(polynomial_without_rook)
-        if length_rook > length_no_rook:
-            for i in range(length_rook - length_no_rook):
-                polynomial_without_rook.append(0)
-
-        if length_no_rook > length_rook:
-            for i in range(length_no_rook - length_rook):
-                polynomial_with_rook.append(0)
-
-        # Now we can just return the sum, which will be the final answer
-        return add_polynomials(polynomial_with_rook, polynomial_without_rook)
 
 def diagonalize_coordinate(board, row, column):
 
@@ -230,7 +136,69 @@ def diagonalize_board(board):
 
     return diagonal_board
 
-def restrict_diagonals(board, row, column):
+def count_rook_cells(board):
+
+    count = 0
+
+    rows = len(board)
+    columns = len(board[0])
+
+    for row in range(rows):
+        for column in range(columns):
+            # Only '1' represents a permissible cell, '0' represents a restricted cell
+            if board[row][column] == '1' or board[row][column] == 'R':
+                count += 1
+    return count
+
+def count_bishop_cells(board):
+
+    count = 0
+
+    rows = len(board)
+    columns = len(board[0])
+
+    for row in range(rows):
+        for column in range(columns):
+            # Only '1' represents a permissible cell, '0' represents a restricted cell
+            if board[row][column] == '1' or board[row][column] == 'B':
+                count += 1
+    return count
+
+def find_first_open_rook_cell(board):
+
+    rows = len(board)
+    columns = len(board[0])
+
+    for row in range(rows):
+        for column in range(columns):
+            if board[row][column] == '1' or board[row][column] == 'R':
+                return [row, column]
+
+    # At this point, there are no vacant, permissible cells
+    return [-1, -1]
+
+def find_first_open_bishop_cell(board):
+
+    rows = len(board)
+    columns = len(board[0])
+
+    for row in range(rows):
+        for column in range(columns):
+            if board[row][column] == '1' or board[row][column] == 'B':
+                return [row, column]
+
+    # At this point, there are no vacant, permissible cells
+    return [-1, -1]
+
+def restrict_rook(board, row, column):
+
+    for c in range(len(board[0])):
+        board[row][c] = '0'
+
+    for r in range(len(board)):
+        board[r][column] = '0'
+
+def restrict_bishop(board, row, column):
 
     rows = len(board)
     columns = len(board[0])
@@ -262,12 +230,78 @@ def restrict_diagonals(board, row, column):
     # Finally, restrict the given cell
     board[row][column] = '0'
 
-def bishop_polynomial(board):
+def restrict_bishop_from_rooks(board, row, column):
 
+    rows = len(board)
+    columns = len(board[0])
+
+    # Any cells that are marked 'R' should not be opened up
+    # to bishops after being restricted from them
+    for c in range(columns):
+        if board[row][c] == '1':
+            board[row][c] = 'B'
+        if board[row][c] == 'R':
+            board[row][c] = '0'
+
+    for r in range(rows):
+        if board[r][column] == '1':
+            board[r][column] = 'B'
+        if board[r][column] == 'R':
+            board[r][column] = '0'
+
+    board[row][column] == '0'
+
+def restrict_rook_from_bishops(board, row, column):
+
+    # Any cells that are marked 'B' should not be opened up
+    # to rooks after being restricted from them
+    rows = len(board)
+    columns = len(board[0])
+
+    # Restrict cells going diagonally down to the right
+    i = 1
+    while row + i < rows and column + i < columns:
+        if board[row + i][column + i] == '1':
+            board[row + i][column + i] = 'R'
+        if board[row + i][column + i] == 'B':
+            board[row + i][column + i] = '0'
+        i = i + 1
+
+    # Restrict cells going diagonally down to the left
+    i = 1
+    while row + i < rows and column - i >= 0:
+        if board[row + i][column - i] == '1':
+            board[row + i][column - i] = 'R'
+        if board[row + i][column - i] == 'B':
+            board[row + i][column - i] = '0'
+        i = i + 1
+
+    # Restrict cells going diagonally up to the left
+    i = 1
+    while row - i >= 0 and column - i >= 0:
+        if board[row - i][column - i] == '1':
+            board[row - i][column - i] = 'R'
+        if board[row - i][column - i] == 'B':
+            board[row - i][column - i] = '0'
+        i = i + 1
+
+    # Restrict cells going diagonally up to the right
+    i = 1
+    while row - i >= 0 and column + i < columns:
+        if board[row - i][column + i] == '1':
+            board[row - i][column + i] = 'R'
+        if board[row - i][column + i] == 'B':
+            board[row - i][column + i] = '0'
+        i = i + 1
+
+def chess_polynomial(board, restrict):
     if count_cells(board) == 0:
         # Base Case: No cells available (1 + 0x = 1)
         return [1]
     elif count_cells(board) == 1:
+        # We need to know what restrictions this cell has to return the appropriate multinomial
+
+
         # Base Case: Only one cell available (1 + x)
         return [1, 1]
     else:
@@ -277,59 +311,183 @@ def bishop_polynomial(board):
 
         # We'll still need to reference this board, so make copies that we 
         # can modify
-        yes_bishop = copy.deepcopy(board)
-        no_bishop = copy.deepcopy(board)
+        yes_piece = copy.deepcopy(board)
+        no_piece = copy.deepcopy(board)
 
         # When placing a rook, restrict it's row and column
-        restrict_diagonals(yes_bishop, cell[0], cell[1])
+        restrict(yes_piece, cell[0], cell[1])
 
         # When not placing a rook, prevent this cell from being used
-        no_bishop[cell[0]][cell[1]] = '0'
+        no_piece[cell[0]][cell[1]] = '0'
 
         # We'll need the rook polynomials for both sub-boards
-        polynomial_with_bishop = bishop_polynomial(yes_bishop)
-        polynomial_without_bishop = bishop_polynomial(no_bishop)
+        polynomial_with_piece = chess_polynomial(yes_piece, restrict)
+        polynomial_without_piece = chess_polynomial(no_piece, restrict)
 
         # Increment the exponents of the rook polynomial for the board we put a rook on
-        polynomial_with_bishop.insert(0, 0)
+        polynomial_with_piece.insert(0, 0)
 
         # We should make sure both lists have the same length to make adding the polynomials 
         # easy
-        length_bishop = len(polynomial_with_bishop)
-        length_no_bishop = len(polynomial_without_bishop)
-        if length_bishop > length_no_bishop:
-            for i in range(length_bishop - length_no_bishop):
-                polynomial_without_bishop.append(0)
+        length_piece = len(polynomial_with_piece)
+        length_no_piece = len(polynomial_without_piece)
+        if length_piece > length_no_piece:
+            for i in range(length_piece - length_no_piece):
+                polynomial_without_piece.append(0)
 
-        if length_no_bishop > length_bishop:
-            for i in range(length_no_bishop - length_bishop):
-                polynomial_with_bishop.append(0)
+        if length_no_piece > length_piece:
+            for i in range(length_no_piece - length_piece):
+                polynomial_with_piece.append(0)
 
         # Now we can just return the sum, which will be the final answer
-        return add_polynomials(polynomial_with_bishop, polynomial_without_bishop)
+        return add_polynomials(polynomial_with_piece, polynomial_without_piece)
 
-if __name__ == "__main__":
+def multinomial_dimension(m0, m1, m2):
 
-    board = load_board(sys.argv[1])
+    # Get the number of rows needed for each multinomial
+    rows_0 = len(m0)
+    rows_1 = len(m1)
+    rows_2 = len(m2)
+    rows = max(rows_0, rows_1, rows_2)
 
-    #print_board(board)
-    #print_polynomial(rook_polynomial(board))
+    # Get the number of columns needed for each multinomial
+    columns_0 = len(m0[0])
+    columns_1 = len(m1[0])
+    columns_2 = len(m2[0])
+    columns = max(columns_0, columns_1, columns_2)
 
-    #diagonal_board = diagonalize_board(board)
-    #print_board(diagonal_board)
+    return [rows, columns]
 
-    #restrict__diagonals(board, 1, 1)
-    #board[1][1] = '0'
-    #print_board(board)
+def normalize_multinomial(m, dimension):
 
-    #restrict_diagonals(board, 2, 2)
-    #print_board(board)
+    # Determine how many rows and columns are needed to match the dimension
+    rows = dimension[0]
+    columns = dimension[1]
 
-    print_board(board)
-    print_polynomial(bishop_polynomial(board))
+    rows_to_add = rows - len(m)
+    columns_to_add = columns - len(m[0])
+
+    # Add the appropriate number of columns
+    for row in m:
+        for c in range(columns_to_add):
+            row.append(0)
+
+    # Each row added must be the same length of the already existant rows in the multinomial
+    blank = []
+    for i in range(len(m[0])):
+        blank.append(0)
+
+    # Add the necessary number of blank lines to the multinomial to get the correct number of 
+    # rows
+    for i in range(rows_to_add):
+        m.append(blank)
+
+def add_multinomials(m0, m1, m2):
+
+    # Initialize an empty multinomial to hold the coefficients
+    s = []
+    dimension = multinomial_dimension(m0, m1, m2)
+    for r in range(dimension[0]):
+        s.append([])
+        for c in range(dimension[1]):
+            s[r].append(0)
+
+    # Perform the addition
+    for r in range(dimension[0]):
+        for c in range(dimension[1]):
+            s[r][c] = m0[r][c] + m1[r][c] + m2[r][c]
+
+    return s
+
+def chess_multinomial(board, restrict_1, restrict_2):
+
+    # Base case: No cells left (1 + 0x + 0y + 0xy)
+    if count_cells(board) == 0:
+
+        return [[1]]
+
+    # With only one cell left, we know what the Chess Polynomial will be
+    elif count_cells(board) == 1:
+
+        # We'll need to know what ipeces this cell is open to
+        [row, column] = find_cell(board)
+
+        if board[row][column] == 'R':
+            return [
+                [1, 1]
+            ]
+
+        elif board[row][column] == 'B':
+            return [
+                [1, 0], 
+                [1, 0]
+            ]
+
+        else:
+            return [
+                [1, 1], 
+                [1, 0]
+            ]
+    else:
+
+        # We need to find an open cell on whioch to decompose
+        # We could decompose on a '1', 'R', or a 'B'
+        cell = find_cell(board)
+
+        # Regardless of what kind of open cell we find, we can always 
+        # choose to restrict it from any pieces in the future
+        no_piece = copy.deepcopy(board)
+        no_piece[cell[0]][cell[1]] = '0'
+        m0 = chess_multinomial(no_piece, restrict_1, restrict_2)
+
+        # If the cell we decompose on is only open to bishops, 
+        # we can't place a rook, so return the 0 multinomial
+        m1 = [[0]]
+        if board[cell[0]][cell[1]] != 'B':
+            piece_1 = copy.deepcopy(board)
+            restrict_1(piece_1, cell[0], cell[1])
+            restrict_rook_from_bishops(piece_1, cell[0], cell[1])
+            m1 = chess_multinomial(piece_1, restrict_1, restrict_2)
+            for polynomial in m1:
+                polynomial.insert(0, 0)
+
+        # If the cell we decompose on is only open to rooks, 
+        # we can't place a bishop, so return the 0 multinomial
+        m2 = [[0]]
+        if board[cell[0]][cell[1]] != 'R':
+            piece_2 = copy.deepcopy(board)
+            restrict_2(piece_2, cell[0], cell[1])
+            restrict_bishop_from_rooks(piece_2, cell[0], cell[1])
+            m2 = chess_multinomial(piece_2, restrict_1, restrict_2)
+            blank_2 = []
+            for z in range(len(m2[0])):
+                blank_2.append(0)
+            m2.insert(0, blank_2)
+
+        # Make sure all multinomials have the same dimension
+        dimension = multinomial_dimension(m0, m1, m2)
+        normalize_multinomial(m0, dimension)
+        normalize_multinomial(m1, dimension)
+        normalize_multinomial(m2, dimension)
+
+        # Add the multinomials together
+        multinomial = add_multinomials(m0, m1, m2)
+
+    return multinomial
+
+def print_multinomial(m):
+
+    [rows, columns] = multinomial_dimension(m, m, m)
+
+    for row in range(rows):
+        for column in range(columns):
+            if row != 0 or column != 0:
+                print(" + ", end = "")
+            if m[row][column] != 0:
+                print(str(m[row][column]) + "(r^" + str(column) + ")(b^" + str(row) + ")", end = "")
 
     print()
-    diagonal_board = diagonalize_board(board)
-    print_board(diagonal_board)
-    print_polynomial(rook_polynomial(diagonal_board))
-    
+
+if __name__ == "__main__":
+    board = load_board(sys.argv[1])
+    print_multinomial(chess_multinomial(board, restrict_rook, restrict_bishop))
